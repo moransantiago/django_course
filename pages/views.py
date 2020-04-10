bbf126d605905cf92edfb09a03ea1ce180bfe225
moransantiago <santimoran.1@hotmail.com>
2020-04-10 12:37:06 -0300

    Template engine tags and syntax added

--- pages/views.py
+++ pages/views.py
@@ -6,16 +6,22 @@ from django.shortcuts import render
 
 def about_view(request, *args, **kwargs):
     # return HttpResponse('<h1>Hello world</h1>') # string of HTML code
-     # The second parameter of render is the template, registered inside TEMPLATES array on project settings.py
-    return render(request, 'about.html', {})
+    # The second parameter of render is the template, registered inside TEMPLATES array on project settings.py
+    my_context = {
+        'my_title': 'this is about us',
+        'this_is_true': True,
+        'my_number': 123,
+        'my_list': [1, 2, 312, 4, 'abc'],
+        'my_html': '<h1>Hello world</h1>'
+    }
+    return render(request, 'about.html', my_context)
 
 
 def contact_view(request, *args, **kwargs):
-     # The second parameter of render is the template, registered inside TEMPLATES array on project settings.py
+    # The second parameter of render is the template, registered inside TEMPLATES array on project settings.py
     return render(request, 'contact.html', {})
 
 
 def home_view(request, *args, **kwargs):
-     # The second parameter of render is the template, registered inside TEMPLATES array on project settings.py
+    # The second parameter of render is the template, registered inside TEMPLATES array on project settings.py
     return render(request, 'home.html', {})
-
