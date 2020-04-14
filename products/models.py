b3fb46529f7a80925cd88305217a987382c12fb1
moransantiago <santimoran.1@hotmail.com>
2020-04-14 11:19:38 -0300

    Introducing to django forms

--- products/models.py
+++ products/models.py
@@ -8,4 +8,4 @@ class Product(models.Model):
     description = models.TextField(blank = True, null = True)
     price = models.DecimalField(decimal_places = 2, max_digits = 1000)
     summary = models.TextField(blank = True, null = False)
-    featured = models.BooleanField()
+    featured = models.BooleanField(default=False)
--- products/views.py
+++ products/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 
+from .forms import ProductForm
+
 from .models import Product
 
 # Create your views here.
+
+
+# HOW TO MAKE A DJANGO FORM WITH RAW HTML
+# def product_create_view(req):
+#     if req.method == 'POST':
+#         title = req.POST['title']
+#         description = req.POST['description']
+#         price = req.POST['price']
+#         Product.objects.create(title=title, description=description, price=price)
+#     context = {}
+# return render(req, 'products/product_create.html', context)
+
+
+# HOW TO MAKE A DJANGO FORM WITH AUTO VALIDATION
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'products/product_create.html', context)
 
 
 def product_detail_view(request):
@@ -14,4 +40,4 @@ def product_detail_view(request):
     context = {
         'product': product
     }
-    return render(request, 'product/detail.html', context)
+    return render(request, 'products/product_detail.html', context)
