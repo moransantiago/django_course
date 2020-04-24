e5c5fb468ccdd3610378d6cf846fe6ebb6a20f76
moransantiago <santimoran.1@hotmail.com>
2020-04-24 12:20:01 -0300

    Class based views added!!

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 
-from blog.views import article_list, article_detail
+from blog.views import (
+    ArticleListView,
+    ArticleDetailView,
+    ArticleCreateView,
+    ArticleUpdateView,
+    ArticleDeleteView
+)
 
 app_name = 'blog'
 urlpatterns = [
-    path('', article_list, name='article_list'),
-    path('<int:id>/', article_detail, name='article_detail'),
+    path('', ArticleListView.as_view(), name='article_list'),
+    path('<int:id>/', ArticleDetailView.as_view(), name='article_detail'),
+    path('create/', ArticleCreateView.as_view(), name='article_create'),
+    path('update/<int:id>', ArticleUpdateView.as_view(), name='article_update'),
+    path('delete/<int:id>', ArticleDeleteView.as_view(), name='article_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,54 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import (
+    CreateView,
+    DetailView,
+    ListView,
+    UpdateView,
+    DeleteView
+)
 
+from .forms import ArticleForm
 from .models import Article
 
 
-def article_list(req):
+class ArticleListView(ListView):
+    template_name = 'blog/article_list.html'
     queryset = Article.objects.all()
-    context = {
-        'article_list': queryset
-    }
-
-    return render(req, 'blog/article_list.html', context)
 
 
-def article_detail(req, id):
-    article = get_object_or_404(Article, id=id)
-    context = {
-        'article': article
-    }
+class ArticleDetailView(DetailView):
+    template_name = 'blog/article_detail.html'
 
-    return render(req, 'blog/article_detail.html', context)
+    def get_object(self):
+        id = self.kwargs.get('id')
+        return get_object_or_404(Article, id=id)
+
+
+class ArticleCreateView(CreateView):
+    form_class = ArticleForm
+    template_name = 'blog/article_create.html'
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+class ArticleUpdateView(UpdateView):
+    form_class = ArticleForm
+    template_name = 'blog/article_create.html'
+
+    def get_object(self):
+        id = self.kwargs.get('id')
+        return get_object_or_404(Article, id=id)
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+class ArticleDeleteView(DeleteView):
+    template_name = 'blog/article_delete.html'
+
+    def get_object(self):
+        id = self.kwargs.get('id')
+        return get_object_or_404(Article, id=id)
+
+    def get_success_url(self):
+        return reverse('blog:article_list')
